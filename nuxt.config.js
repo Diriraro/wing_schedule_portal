43172014af7c86c25bd2ba2b7e82bedf5fc39f7e
Diriraro <94skyoun@naver.com>
2022-01-06 17:10:27 +0900

    test apply dashboard

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,21 +24,46 @@ export default {
       },
       { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css",
+      },
+    ],
+    bodyAttrs: {
+      class: "white-content", // Add `white-content` class here to enable "white" mode.
+    },
+  },
+  router: {
+    linkExactActiveClass: "active",
   },
 
   mode: "universal",
 
+  loading: { color: "#fff" },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["swiper/css/swiper.css"],
+  // css: ["swiper/css/swiper.css"],
+
+  css: [
+    "assets/css/demo.css",
+    "assets/css/nucleo-icons.css",
+    "assets/sass/black-dashboard.scss",
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "~/plugins/polyfill.js",
     "~/plugins/utils.js",
     "~/plugins/vue-cookies.js",
+    `~/plugins/dashboard-plugin.js`,
     { src: "~/plugins/smooth-scroll.js", ssr: false },
-    { src: "~/plugins/vue-awesome-swiper.js", ssr: false },
+    // { src: "~/plugins/vue-awesome-swiper.js", ssr: false },
     { src: "~/plugins/vue-infinite-scroll.js", ssr: false },
     { src: "~/plugins/vue-clipboard.js", ssr: false },
   ],
@@ -58,7 +83,9 @@ export default {
   modules: [
     ["@nuxtjs/style-resources"],
     "@nuxtjs/device",
-    ["vue-social-sharing/nuxt"],
+    "@nuxtjs/pwa",
+    "nuxt-i18n",
+    // ["vue-social-sharing/nuxt"],
     // ['nuxt-moment', { locale: 'ko' }],
     "nuxt-moment",
   ],
@@ -73,7 +100,18 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   buildDir: "build",
   build: {
+    transpile: [/^element-ui/],
+    // extend(config, ctx) {},
     babel: {
+      plugins: [
+        [
+          "component",
+          {
+            libraryName: "element-ui",
+            styleLibraryName: "theme-chalk",
+          },
+        ],
+      ],
       presets({ envName }, [preset, options]) {
         options.corejs = { version: 3 };
         if (envName === "modern") {
---
+++ plugins/globalComponents copy.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+
+// import Header from '@/components/MVP/Common/Header.vue';
+// import Footer from '@/components/MVP/Common/Footer.vue';
+
+// Vue.component('Header', Header);
+// Vue.component('Footer', Footer);
